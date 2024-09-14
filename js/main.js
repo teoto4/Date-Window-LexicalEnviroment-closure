54c9dc39c6f0f2b4e479b353d3e04a5c455eae93
Revy <162366071+teoto4@users.noreply.github.com>
2024-09-14 23:45:49 +0300

    chore(main.js) - add task 3

--- js/main.js
+++ js/main.js
@@ -39,3 +39,38 @@ function getDay(date) {
 }
 
 console.log(getDay("2004-02-19"));
+
+/*3.(Date)Напишіть функцію, яка вираховує, скільки днів залишилося до наступного Нового року (1 січня).
+Функція повинна враховувати, чи є поточний рік високосним. */
+function dayToNewYear() {
+  const today = new Date();
+  const yearNow = today.getFullYear();
+  const nextYear = yearNow + 1;
+
+  const leapYear = isleapYear(yearNow);
+
+  const nextNewYearDate = new Date(nextYear, 0, 1);
+
+  const millisecondsDiffrence = nextNewYearDate - today;
+
+  const millisecondToDay = Math.ceil(
+    millisecondsDiffrence / (1000 * 60 * 60 * 24)
+  );
+
+  let leap;
+  if (leapYear === true) {
+    leap = "высокосный";
+  } else {
+    leap = "не высокосный";
+  }
+
+  function isleapYear(year) {
+    if (year % 400 == 0) return true;
+    else if (year % 100 == 0) return false;
+    else if (year % 4 == 0) return true;
+    else return false;
+  }
+  return `${millisecondToDay} дней до нового года - Сейчас ${leap} год`;
+}
+
+console.log(dayToNewYear());
